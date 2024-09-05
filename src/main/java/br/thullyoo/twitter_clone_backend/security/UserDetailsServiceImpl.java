161369f41b67bff50905062a4352115fdec102a1
Thullyoo <thullyofilho@gmail.com>
2024-09-05 18:41:11 -0300

    feat: created UserDetails and UserDetailsService

---
+++ src/main/java/br/thullyoo/twitter_clone_backend/security/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package br.thullyoo.twitter_clone_backend.security;
+
+import br.thullyoo.twitter_clone_backend.domain.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.findByUsername(username)
+                .map(UserDetailsImpl::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+    }
+}
