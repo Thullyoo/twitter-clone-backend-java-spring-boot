a705e9dee31164471e9b99e517bb6956ed6a6e06
Thullyoo <thullyofilho@gmail.com>
2024-09-06 18:54:37 -0300

    fix: added value nickname

--- src/main/java/br/thullyoo/twitter_clone_backend/api/dto/UserResponse.java
+++ src/main/java/br/thullyoo/twitter_clone_backend/api/dto/UserResponse.java
@@ -2,5 +2,5 @@ package br.thullyoo.twitter_clone_backend.api.dto;
 
 import java.util.UUID;
 
-public record UserResponse(UUID id, String email, String name, String password) {
+public record UserResponse(UUID id, String email,String nickname, String name, String password) {
 }
