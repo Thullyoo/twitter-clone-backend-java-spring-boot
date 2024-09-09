08ada6a0cef06baed18c684fd192fd28afcccc41
Thullyoo <thullyofilho@gmail.com>
2024-09-09 16:53:34 -0300

    feat: controller and service for delete tweet

--- src/main/java/br/thullyoo/twitter_clone_backend/api/controllers/TweetController.java
+++ src/main/java/br/thullyoo/twitter_clone_backend/api/controllers/TweetController.java
@@ -3,12 +3,13 @@ package br.thullyoo.twitter_clone_backend.api.controllers;
 import br.thullyoo.twitter_clone_backend.api.dto.TweetRequest;
 import br.thullyoo.twitter_clone_backend.api.dto.TweetResponse;
 import br.thullyoo.twitter_clone_backend.domain.service.TweetServiceImpl;
-import br.thullyoo.twitter_clone_backend.service.TweetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController()
 @RequestMapping("/tweet")
@@ -18,12 +19,18 @@ public class TweetController {
     private TweetServiceImpl tweetService;
 
     @PostMapping
-    private ResponseEntity<TweetResponse> registerTweet(@RequestBody TweetRequest tweetRequest){
+    public ResponseEntity<TweetResponse> registerTweet(@RequestBody TweetRequest tweetRequest){
         return ResponseEntity.status(HttpStatus.OK).body(this.tweetService.registerTweet(tweetRequest));
     }
 
     @GetMapping
-    private  ResponseEntity<Page<TweetResponse>> getAllTweets(){
+    public ResponseEntity<Page<TweetResponse>> getAllTweets(){
         return ResponseEntity.status(HttpStatus.OK).body(this.tweetService.getAllTweets());
+    }
+
+    @DeleteMapping("/{tweet_id}/{user_id}")
+    public ResponseEntity deleteTweet(@PathVariable int tweet_id, @PathVariable UUID user_id){
+        this.tweetService.deleteTweet(tweet_id, user_id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/br/thullyoo/twitter_clone_backend/service/TweetService.java
+++ src/main/java/br/thullyoo/twitter_clone_backend/service/TweetService.java
@@ -4,9 +4,13 @@ import br.thullyoo.twitter_clone_backend.api.dto.TweetRequest;
 import br.thullyoo.twitter_clone_backend.api.dto.TweetResponse;
 import org.springframework.data.domain.Page;
 
+import java.util.UUID;
+
 public interface TweetService {
 
     public TweetResponse registerTweet(TweetRequest request);
 
     public Page<TweetResponse> getAllTweets();
+
+    public void deleteTweet(int id_tweet, UUID id_user);
 }
