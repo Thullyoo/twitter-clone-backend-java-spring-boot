46876d2ae2bfe5ad3de5ccc17c6077cfacb234d0
Thullyoo <thullyofilho@gmail.com>
2024-10-05 15:33:53 -0300

    feat: documented controllers

--- src/main/java/br/thullyoo/twitter_clone_backend/api/controllers/TweetController.java
+++ src/main/java/br/thullyoo/twitter_clone_backend/api/controllers/TweetController.java
@@ -3,6 +3,9 @@ package br.thullyoo.twitter_clone_backend.api.controllers;
 import br.thullyoo.twitter_clone_backend.api.dto.TweetRequest;
 import br.thullyoo.twitter_clone_backend.api.dto.TweetResponse;
 import br.thullyoo.twitter_clone_backend.domain.service.TweetServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -12,9 +15,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
-@RestController()
+@RestController
 @RequestMapping("/tweet")
 @CrossOrigin(origins = "http://localhost:4200")
+@Tag(name = "Tweet", description = "Controller for registering, deleting, and retrieving tweets")
 public class TweetController {
 
     @Autowired
@@ -22,20 +26,36 @@ public class TweetController {
 
     @PostMapping
     @PreAuthorize("hasAuthority('SCOPE_common')")
-    public ResponseEntity<TweetResponse> registerTweet(@RequestBody TweetRequest tweetRequest){
+    @Operation(summary = "Save user tweet", description = "Method to register a user's tweet")
+    @ApiResponse(responseCode = "200", description = "Tweet successfully registered")
+    @ApiResponse(responseCode = "401", description = "Permission error")
+    @ApiResponse(responseCode = "403", description = "Authentication error")
+    @ApiResponse(responseCode = "404", description = "User not found")
+    public ResponseEntity<TweetResponse> registerTweet(@RequestBody TweetRequest tweetRequest) {
         return ResponseEntity.status(HttpStatus.OK).body(this.tweetService.registerTweet(tweetRequest));
     }
 
     @GetMapping
     @PreAuthorize("hasAuthority('SCOPE_common') or hasAuthority('SCOPE_admin')")
-    public ResponseEntity<Page<TweetResponse>> getAllTweets(){
+    @Operation(summary = "Retrieve all tweets", description = "Method to retrieve all tweets")
+    @ApiResponse(responseCode = "200", description = "Tweets successfully retrieved")
+    @ApiResponse(responseCode = "401", description = "Permission error")
+    @ApiResponse(responseCode = "403", description = "Authentication error")
+    @ApiResponse(responseCode = "404", description = "User not found")
+    public ResponseEntity<Page<TweetResponse>> getAllTweets() {
         return ResponseEntity.status(HttpStatus.OK).body(this.tweetService.getAllTweets());
     }
 
     @DeleteMapping("/{tweet_id}/{user_id}")
     @PreAuthorize("hasAuthority('SCOPE_common') or hasAuthority('SCOPE_admin')")
-    public ResponseEntity deleteTweet(@PathVariable int tweet_id, @PathVariable UUID user_id){
+    @Operation(summary = "Delete user tweet", description = "Method to delete a user's tweet")
+    @ApiResponse(responseCode = "204", description = "Tweet successfully deleted")
+    @ApiResponse(responseCode = "401", description = "Permission error")
+    @ApiResponse(responseCode = "403", description = "Authentication error")
+    @ApiResponse(responseCode = "404", description = "Tweet or user not found")
+    public ResponseEntity<Void> deleteTweet(@PathVariable int tweet_id, @PathVariable UUID user_id) {
         this.tweetService.deleteTweet(tweet_id, user_id);
         return ResponseEntity.noContent().build();
     }
 }
+
--- src/main/java/br/thullyoo/twitter_clone_backend/api/controllers/UserController.java
+++ src/main/java/br/thullyoo/twitter_clone_backend/api/controllers/UserController.java
@@ -4,6 +4,9 @@ import br.thullyoo.twitter_clone_backend.api.dto.JwtResponse;
 import br.thullyoo.twitter_clone_backend.api.dto.UserRequest;
 import br.thullyoo.twitter_clone_backend.api.dto.UserResponse;
 import br.thullyoo.twitter_clone_backend.domain.service.UserServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,18 +15,25 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
+@Tag(name = "User", description = "Controller for registering and logging in Users")
 public class UserController {
 
     @Autowired
     private UserServiceImpl userService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponse> registerUser(@RequestBody UserRequest userRequest){
+    @Operation(summary = "Register User", description = "Method to register a user")
+    @ApiResponse(responseCode = "201", description = "User successfully registered")
+    @ApiResponse(responseCode = "400", description = "Email or Nickname already in use")
+    public ResponseEntity<UserResponse> registerUser(@RequestBody UserRequest userRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(this.userService.registerUser(userRequest));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<JwtResponse> loginUser(Authentication authentication){
+    @Operation(summary = "User Login", description = "Method for user login")
+    @ApiResponse(responseCode = "200", description = "Login successful")
+    @ApiResponse(responseCode = "401", description = "Incorrect username or password")
+    public ResponseEntity<JwtResponse> loginUser(Authentication authentication) {
         return ResponseEntity.status(HttpStatus.OK).body(this.userService.loginUser(authentication));
     }
 }
