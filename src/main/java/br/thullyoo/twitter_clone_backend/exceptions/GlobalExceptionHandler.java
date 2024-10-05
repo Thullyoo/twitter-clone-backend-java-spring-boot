1a6d42a64c0e3c3b968897b2934256a7f45b5561
Thullyoo <thullyofilho@gmail.com>
2024-10-05 12:31:55 -0300

    feat: impl global exception handler

--- src/main/java/br/thullyoo/twitter_clone_backend/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/thullyoo/twitter_clone_backend/exceptions/GlobalExceptionHandler.java
@@ -9,5 +9,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ExceptionResponseDTO> userNotFoundException(String message){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionResponseDTO(HttpStatus.NOT_FOUND, message));
+    }
 
+    @ExceptionHandler(TweetNotFoundException.class)
+    public ResponseEntity<ExceptionResponseDTO> tweetNotFoundException(String message){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionResponseDTO(HttpStatus.NOT_FOUND, message));
+    }
+
+    @ExceptionHandler(EmailAlreadyRegisteredException.class)
+    public ResponseEntity<ExceptionResponseDTO> emailAlreadyRegisteredException(String message){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionResponseDTO(HttpStatus.BAD_REQUEST, message));
+    }
+
+    @ExceptionHandler(NicknameAlreadyRegisteredException.class)
+    public ResponseEntity<ExceptionResponseDTO> nicknameAlreadyRegisteredException(String message){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ExceptionResponseDTO(HttpStatus.BAD_REQUEST, message));
+    }
 }
